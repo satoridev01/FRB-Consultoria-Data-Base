93e1f88c25bd7bcfd70a11ba73ddc9da0e1d03b5
Lucas <lucaspedrosadesouza@gmail.com>
2023-05-26 21:20:29 -0300

    tes

--- databasefrb/settings.py
+++ databasefrb/settings.py
@@ -195,4 +195,4 @@ AWS_S3_REGION_NAME = "us-east-2"
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
-DEFAULT_FILE_STORAGE = "storages.databasefrb.s3boto3.S3Boto3Storage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
