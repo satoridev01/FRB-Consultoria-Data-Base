ddef1e1177658755de99bf7810153b142002b5df
Lucas <lucaspedrosadesouza@gmail.com>
2023-05-29 11:35:07 -0300

    foi

--- googleDataBase.py
+++ googleDataBase.py
@@ -1,13 +1,13 @@
-# Imports the Google Cloud client library
-from google.cloud import storage
+# # Imports the Google Cloud client library
+# from google.cloud import storage
 
-# Instantiates a client
-storage_client = storage.Client()
+# # Instantiates a client
+# storage_client = storage.Client()
 
-# The name for the new bucket
-bucket_name = "my-new-bucket"
+# # The name for the new bucket
+# bucket_name = "my-new-bucket"
 
-# Creates the new bucket
-bucket = storage_client.create_bucket(bucket_name)
+# # Creates the new bucket
+# bucket = storage_client.create_bucket(bucket_name)
 
-print(f"Bucket {bucket.name} created.")
+# print(f"Bucket {bucket.name} created.")
